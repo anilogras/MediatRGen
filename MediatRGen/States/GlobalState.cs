dad7057c41e6fa7ebf1583179dae6fbfe39fdb95
anilogras <69460578+anilogras@users.noreply.github.com>
2025-03-21 09:41:30 +0300

    Global state

---
+++ MediatRGen/States/GlobalState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRGen.States
+{
+    public class GlobalState
+    {
+        public GlobalState()
+        {
+            Lang = "en";
+        }
+
+        private static GlobalState _instance;
+
+        public static GlobalState instance
+        {
+            get { 
+            
+                if(_instance == null)
+                    _instance = new GlobalState();
+
+                return _instance;
+            }
+        }
+
+        public string Lang { get; set; }
+    }
+}
