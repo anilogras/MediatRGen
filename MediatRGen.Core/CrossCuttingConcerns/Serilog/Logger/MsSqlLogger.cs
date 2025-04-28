4b9e5e85cfc403edf930a26b852104da12d74e3e
anilogras <69460578+anilogras@users.noreply.github.com>
2025-04-28 08:47:03 +0300

    core files added

---
+++ MediatRGen.Core/CrossCuttingConcerns/Serilog/Logger/MsSqlLogger.cs
@@ -0,0 +1,38 @@
+﻿using MediatRGen.Core.CrossCuttingConcerns.Serilog;
+using MediatRGen.Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using MediatRGen.Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRGen.Core.CrossCuttingConcerns.Serilog.Logger;
+
+//https://github.com/serilog-mssql/serilog-sinks-mssqlserver
+public class MsSqlLogger : LoggerServiceBase
+{
+    public MsSqlLogger(IConfiguration configuration)
+    {
+        MsSqlConfiguration logConfiguration =
+            configuration.GetSection("SeriLogConfigurations:MsSqlConfiguration").Get<MsSqlConfiguration>()
+            ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        MSSqlServerSinkOptions sinkOptions = new()
+        {
+            TableName = logConfiguration.TableName,
+            AutoCreateSqlDatabase = logConfiguration.AutoCreateSqlTable
+        };
+
+        ColumnOptions columnOptions = new();
+
+        global::Serilog.Core.Logger seriLogConfig = new LoggerConfiguration().WriteTo
+            .MSSqlServer(logConfiguration.ConnectionString, sinkOptions, columnOptions: columnOptions)
+            .CreateLogger();
+
+        Logger = seriLogConfig;
+    }
+}
---
+++ MediatRGen.Core/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,14 @@
+﻿using MediatRGen.Core.Security.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRGen.Core.Security.JWT;
+
+public interface ITokenHelper
+{
+    AccessToken CreateToken(User user, IList<OperationClaim> operationClaims);
+    RefreshToken CreateRefreshToken(User user, string ipAddress);
+}
