8fcfb10faf417ee091bf7af3b9e85a700c732a0b
Anıl OĞRAŞ <forkboysf1@hotmail.com>
2025-05-20 22:42:42 +0300

    file service added

---
+++ MediatRGen.Services/HelperServices/FileService.cs
@@ -0,0 +1,167 @@
+﻿using MediatRGen.Core;
+using MediatRGen.Core.Exceptions.FileExceptions;
+using MediatRGen.Core.States;
+using MediatRGen.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MediatRGen.Services.HelperServices
+{
+    public class FileService
+    {
+        public static ServiceResult<bool> Create(string path, string fileName, string content)
+        {
+            try
+            {
+                string _path = DirectoryServices.GetPath(path, fileName).Value;
+
+                if (!File.Exists(_path))
+                {
+                    File.WriteAllText(_path, content);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>(false, false, LangHandler.Definitions().FileCreateError, new FileException(ex.Message));
+            }
+
+            return new ServiceResult<bool>(true, true, LangHandler.Definitions().FileCreated + $" ({fileName})", null);
+
+        }
+        public static ServiceResult<bool> Create(string path, string fileName, object content)
+        {
+            try
+            {
+                string _path = DirectoryServices.GetPath(path, fileName).Value;
+                if (!File.Exists(_path))
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true
+                    };
+
+                    var jsonString = JsonSerializer.Serialize(content, options);
+
+                    File.WriteAllText(_path, jsonString);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>(false, false, LangHandler.Definitions().FileCreateError, new FileException(ex.Message));
+            }
+
+            return new ServiceResult<bool>(true, true, LangHandler.Definitions().FileCreated + $" ({fileName})", null);
+        }
+
+        public static ServiceResult<bool> Create(string path, string fileName)
+        {
+            try
+            {
+                string _path = DirectoryServices.GetPath(path, fileName).Value;
+
+                if (!File.Exists(_path))
+                {
+                    File.Create(_path).Close();
+                    Console.WriteLine(LangHandler.Definitions().FileCreated + $" ({fileName})");
+                }
+
+                return new ServiceResult<bool>(true, true, "", null);
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>(false, false, LangHandler.Definitions().FileCreateError, new FileException(ex.Message));
+            }
+        }
+
+        public static ServiceResult<string?> Get(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    string _file = File.ReadAllText(path, Encoding.UTF8);
+                    return new ServiceResult<string?>(_file, true, "", null);
+                }
+
+                return new ServiceResult<string?>(null, false, LangHandler.Definitions().FileNotFound, new FileException(LangHandler.Definitions().FileNotFound));
+
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<string?>(null, false, LangHandler.Definitions().FileReadError, new FileException(ex.Message));
+            }
+        }
+
+        public static ServiceResult<bool> UpdateConfig()
+        {
+
+            try
+            {
+                string _configPath = DirectoryServices.GetPath(DirectoryServices.GetCurrentDirectory().Value, GlobalState.ConfigFileName).Value;
+
+                string? _file = Get(_configPath).Value;
+
+                if (string.IsNullOrEmpty(_file) == true)
+                {
+                    throw new FileException(LangHandler.Definitions().ConfigNotFound);
+                }
+
+                File.Delete(_configPath);
+
+                Create(DirectoryServices.GetCurrentDirectory().Value, GlobalState.ConfigFileName, GlobalState.Instance);
+
+                return new ServiceResult<bool>(true, true, LangHandler.Definitions().ConfigUpdated, null);
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>(false, false, LangHandler.Definitions().ConfigUpdateError, new FileException(ex.Message));
+            }
+        }
+
+        public static ServiceResult<string> FindFileRecursive(string directory, string targetFile)
+        {
+            try
+            {
+                foreach (var file in Directory.GetFiles(directory))
+                {
+                    if (Path.GetFileName(file).Equals(targetFile, StringComparison.OrdinalIgnoreCase))
+                        return new ServiceResult<string>(file, true, "");
+                }
+
+                foreach (var dir in Directory.GetDirectories(directory))
+                {
+                    string found = FindFileRecursive(dir, targetFile).Value;
+                    if (found != null)
+                        return new ServiceResult<string>(found, true, "");
+                }
+
+                return new ServiceResult<string>(string.Empty, false, LangHandler.Definitions().FileNotFound, new FileException(LangHandler.Definitions().FileNotFound));
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<string>(string.Empty, false, LangHandler.Definitions().FileNotFound, new FileException(ex.Message));
+            }
+        }
+
+        public static ServiceResult<bool> CheckFile(string path, string fileName)
+        {
+            try
+            {
+                string _combinedPathWithFile = DirectoryServices.GetPath(path, fileName).Value;
+
+                if (File.Exists(_combinedPathWithFile))
+                    return new ServiceResult<bool>(true, true, LangHandler.Definitions().FileFounded);
+
+                return new ServiceResult<bool>(false, true, LangHandler.Definitions().FileNotFound);
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>(false, false, LangHandler.Definitions().FileFindError, new FileException(ex.Message));
+            }
+        }
+    }
+}
