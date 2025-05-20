198dc9780c63296382bfa3defcc7c833b0f1d078
Anıl OĞRAŞ <forkboysf1@hotmail.com>
2025-05-20 23:38:23 +0300

    .

---
+++ MediatRGen.Services/HelperServices/ClassService.cs
@@ -0,0 +1,122 @@
+﻿using MediatRGen.Core.Exceptions;
+using MediatRGen.Services.Base;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRGen.Services.HelperServices
+{
+    public class ClassService
+    {
+
+
+        hatalı ,  serviceli hale getir.
+        public static string AddNewProperty(string classString, string propertyName, SyntaxKind propertyType, bool getProp = true, bool setProp = true)
+        {
+            var createCs = CSharpSyntaxTree.ParseText(classString);
+            var root = createCs.GetRoot();
+            var classDeclaration = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First();
+
+            var accessors = new List<AccessorDeclarationSyntax>();
+
+            var newProperty = SyntaxFactory.PropertyDeclaration(SyntaxFactory.PredefinedType(SyntaxFactory.Token(propertyType)), propertyName)
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .NormalizeWhitespace();
+
+            if (getProp == true)
+            {
+                accessors.Add(
+                    SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                        .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
+                    );
+            }
+
+            if (setProp == true)
+            {
+                accessors.Add(
+                    SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                        .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
+                    );
+            }
+
+            if (accessors.ToArray().Length > 0)
+                newProperty = newProperty.AddAccessorListAccessors(accessors.ToArray());
+            else
+                newProperty = newProperty.WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken));
+
+            var updatedClassDeclaration = classDeclaration.AddMembers(newProperty);
+
+            // Root'u güncelliyoruz
+            root = root.ReplaceNode(classDeclaration, updatedClassDeclaration);
+            return root.NormalizeWhitespace().ToFullString();
+        }
+        public static void ChangeNameSpace(string classPath, string newNameSpace)
+        {
+            SyntaxNode root = GetClassRoot(classPath);
+
+            newNameSpace = newNameSpace.Substring(newNameSpace.IndexOf("\\src\\") + 5);
+            newNameSpace = newNameSpace.Substring(newNameSpace.IndexOf("\\") + 1);
+            newNameSpace = newNameSpace.Replace("\\", ".");
+
+
+            var fileScopedNs = root.DescendantNodes()
+                .OfType<FileScopedNamespaceDeclarationSyntax>()
+                .FirstOrDefault();
+
+            var blockNs = root.DescendantNodes()
+                .OfType<NamespaceDeclarationSyntax>()
+                .FirstOrDefault();
+
+            var namespaceNode = (BaseNamespaceDeclarationSyntax?)fileScopedNs ?? blockNs;
+
+            if (namespaceNode != null)
+            {
+                var newNamespace = SyntaxFactory.ParseName(newNameSpace);
+                var newNamespaceNode = namespaceNode.WithName(newNamespace);
+                var newRoot = root.ReplaceNode(namespaceNode, newNamespaceNode);
+
+                ReWriteClass(classPath, newRoot);
+            }
+            else
+            {
+                throw new SystemProcessException(LangHandler.Definitions().NameSpaceNotFound);
+            }
+        }
+        public static void SetBaseInheritance(string classPath, string baseClassName)
+        {
+            SyntaxNode root = GetClassRoot(classPath).Value;
+            var classNode = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First();
+            var baseType = SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName(baseClassName));
+            var baseList = SyntaxFactory.BaseList(SyntaxFactory.SeparatedList<BaseTypeSyntax>().Add(baseType));
+            var newClassNode = classNode.WithBaseList(baseList);
+            var newRoot = root.ReplaceNode(classNode, newClassNode);
+            ReWriteClass(classPath, newRoot);
+        }
+        private static void ReWriteClass(string classPath, SyntaxNode newRoot)
+        {
+            var updatedCode = newRoot.NormalizeWhitespace().ToFullString();
+            File.WriteAllText(classPath + ".cs", updatedCode);
+        }
+        private static ServiceResult<SyntaxNode> GetClassRoot(string classPath)
+        {
+            string? extension = Path.GetExtension(classPath);
+
+            if (string.IsNullOrEmpty(extension))
+                classPath = classPath + ".cs";
+
+            string? _classText = FileService.Get(classPath).Value;
+
+            if (string.IsNullOrEmpty(_classText))
+                return new ServiceResult<SyntaxNode>(null, false, LangHandler.Definitions().ClassNotFound);
+
+            var tree = CSharpSyntaxTree.ParseText(_classText);
+            var root = tree.GetRoot();
+            return new ServiceResult<SyntaxNode>(root, true, "");
+        }
+    }
+}
