4a34c921562e4c4e6eb094edd1f19c0d17ee7b9b
Anıl OĞRAŞ <forkboysf1@hotmail.com>
2025-05-20 22:47:57 +0300

    nugetservice added

---
+++ MediatRGen.Services/NugetServices/NugetService.cs
@@ -0,0 +1,32 @@
+﻿using MediatRGen.Core;
+using MediatRGen.Core.Helpers;
+using MediatRGen.Services.Base;
+using MediatRGen.Services.HelperServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRGen.Services.NugetServices
+{
+    public class NugetService
+    {
+        public NugetService()
+        {
+
+        }
+
+        public ServiceResult<bool> DeleteNugets()
+        {
+            string _nugetFoldersDirectory = DirectoryServices.GetPath(DirectoryServices.GetCurrentDirectory().Value, "CoreNugetPackages").Value;
+            ServiceResult<bool> deleteRes = DirectoryServices.Delete(_nugetFoldersDirectory);
+            return new ServiceResult<bool>(true, true, LangHandler.Definitions().NugetPackageDeleted);
+        }
+
+
+
+    }
+
+
+}
