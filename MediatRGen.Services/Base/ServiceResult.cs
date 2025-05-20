dbbc75b7da494ae6ef7006eb193c8c448b6b2d42
Anıl OĞRAŞ <forkboysf1@hotmail.com>
2025-05-20 23:17:22 +0300

    .

--- MediatRGen.Services/Base/ServiceResult.cs
+++ MediatRGen.Services/Base/ServiceResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatRGen.Core.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,42 @@ namespace MediatRGen.Services.Base
     public class ServiceResult<T>
     {
 
-        public ServiceResult(T _value, bool status)
+        public ServiceResult(T _value, bool status, string message)
         {
             Value = _value;
             Status = status;
+            Message = message;
+            InvokeResult();
         }
 
+        public ServiceResult(T _value, bool status, string message, Exception? error)
+        {
+            Value = _value;
+            Status = status;
+            Message = message;
+            Error = error;
+            InvokeResult();
+        }
 
         public T Value { get; set; }
         public bool Status { get; set; }
-        public List<ServiceError> Errors { get; set; }
-    }
+        public string Message { get; set; }
+        public Exception? Error { get; set; }
 
-    public class ServiceError
-    {
-        public string Error { get; set; }
+        private void InvokeResult()
+        {
+            if (Error != null)
+            {
+                if (Message is not "")
+                    Console.Write(Message);
+
+                throw Error;
+
+            }
+            else
+                Console.Write(Message);
+        }
+
     }
 
 }
