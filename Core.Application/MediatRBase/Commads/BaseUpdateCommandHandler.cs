ef081b6af6b1232d6d8442edaaed99e6160b69b5
anilogras <69460578+anilogras@users.noreply.github.com>
2025-05-23 13:12:42 +0300

    basecommands added

---
+++ Core.Application/MediatRBase/Commads/BaseUpdateCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.MediatRBase.Commads
+{
+    public class BaseUpdateCommandHandler<TRequest, TResponse, TEntity> : IRequestHandler<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TEntity : class, new()
+    {
+        public virtual Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core.Application/MediatRBase/Queries/GetByIdBaseCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.MediatRBase.Queries
+{
+    public class GetByIdBaseCommand<TRequest, TResponse, TEntity> : IRequestHandler<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TEntity : class, new()
+    {
+        public virtual Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
