d117e671d9b4b34f69df45b99115bce02bd82856
anilogras <69460578+anilogras@users.noreply.github.com>
2025-05-23 14:20:00 +0300

    .

---
+++ Core.Deneme/TempDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Deneme.Features;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Deneme
+{
+    public class TempDbContext : DbContext
+    {
+        public DbSet<Isyeri> Isyeri { get; set; }
+    }
+}
